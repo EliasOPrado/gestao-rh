2ebd92fd4e122f7e1e0a85f646bca7db8780fdf8
EliasOPrado <eliaspradoprofessional@outlook.com>
2022-04-10 18:41:27 -0300

    chore: Added link between empresa and staff.

--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic.edit import CreateView
 from apps.empresas.models import Empresa
 
@@ -6,4 +7,10 @@ from apps.empresas.models import Empresa
 class EmpresaCreate(CreateView):
     model = Empresa
     fields = ['nome', ]
-    
+
+    def form_valid(self, form):
+        obj = form.save()
+        funcionario = self.request.user.funcionario
+        funcionario.empresa = obj
+        funcionario.save()
+        return HttpResponse('Ok')
